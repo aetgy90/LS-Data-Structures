c468c648e7142eb2229ef119645ff5cd688c8fe1
Nana Owusu Nyamekye <nanaowusu1234@gmail.com>
2017-07-14 23:45:45 +0000

    'queue.js' Completed

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,19 @@
 class Queue {
+  constructor() {
+    this.holder = [];
+  }
 
+  enqueue(someArgs) {
+    return this.holder.push(someArgs);
+  }
+
+  dequeue() {
+    return this.holder.shift();
+  }
+
+  get size() {
+    return this.holder.length;
+  }
 }
 
 module.exports = Queue;
